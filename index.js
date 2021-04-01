81fe1a3b7054f8ac7c0ec451e208878376445c49
QkeleQ10 <quintenra@gmail.com>
2021-04-01 16:35:33 +0200

    language picker stuff and better menu

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function start(t) {
     langCode = langCookie || window.navigator.language || "en"
     if (t > 1) langCode = window.navigator.language.split("-")[0] || "en"
     if (t > 2) langCode = "en"
-    if (t > 3) {
+    if (t > 3) { window.location.href = window.location.href.split('?')[0] }
+    if (t > 4) {
         ts.innerHTML = "Couldn't load troubleshooter."
         return console.error("Loading steps: Aborted.")
     }
@@ -58,6 +59,30 @@ function localise(t) {
             console.log("Loading strings: Failed.")
             localise(t + 1)
         })
+}
+
+function openlangpicker() {
+    let e = document.getElementById("langbutton")
+    let langs = [["en", "English"], ["nl", "Nederlands"], ["de", "Deutsch"], ["fr", "Français"], ["pt", "Português"], ["pl", "Polski"], ["ru", "Русский"], ["tr", "Türkçe"], ["id", "Bahasa Indonesia"], ["ms", "Bahasa Melayu"]]
+
+    let p = document.getElementById("langpopup")
+    p.classList.remove("hidden")
+    document.querySelector(".popupbk").classList.remove("hidden")
+    document.getElementById("langlist").innerHTML = ""
+
+    langs.forEach((l) => {
+        let b = document.createElement("a")
+        b.id = l[0]
+        if (langCode === l[0]) b.setAttribute("disabled", true)
+        b.innerHTML = l[1]
+        b.setAttribute("onclick", "picklang(this.id)")
+        document.getElementById("langlist").appendChild(b)
+    })
+}
+
+function picklang(lang) {
+    document.cookie = `lang=${lang}`
+    window.location.reload()
 }
 
 
